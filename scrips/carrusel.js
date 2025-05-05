c1416cf2d7c196b8bcfe90e52fb9f2c21849e2f8
JuanDavidVA <juandavidvalencia@paucasesnovescifp.cat>
2025-05-05 13:02:01 +0200

    Commit: cambios realizados para el blog

--- scrips/carrusel.js
+++ scrips/carrusel.js
@@ -1,7 +1,7 @@
 let descriptions = [
-    "Esta es la descripción de la imagen 1.Lorem, ipsum dolor sit amet consectetur adipisicing elit. ",
-    "Esta es la descripción de la imagen central.Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-    "Esta es la descripción de la imagen 3.Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+    "Se utiliza una iluminación simple, al igual que unos gráficos minimalistas.Los efectos de sonido y la música se utilizan para dar la sensación de estrés que pretende el juego, al mismo tiempo que favorecen a la jugabilidad dando señales acústicas y advertencias que favorecen al jugador durante la partida.",
+    "El modelo del dron es de creación propia, ajustándose a los colores y la ambientación principal que tiene el juego.Las mecánicas del dron y su original forma de manejarlo por el entorno mediante las cámaras de seguridad, son la principal esencia por la que se caracteriza el juego y es lo que más trabajo ha conllevado a la hora de su desarrollo. Teniendo el resultado óptimo y viable para el juego.",
+    "Desde un inicio se pretendía darle más protagonismo a la criatura, dándole otro diseño diferente y haciéndolo más peligroso teniendo más interacciones durante la partida. Pero por problemas de desarrollo no se le ha podido hacer mucho más.Tal vez más adelante reciba un importante rework."
 ];
 
 function changeText(index) {
